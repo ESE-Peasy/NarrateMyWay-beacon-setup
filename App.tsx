5bd5b17bfda9c8f1853b64b648b6a3ed7658d419
mhr <2247469r@student.gla.ac.uk>
2021-03-04 18:42:34 +0000

    Function based approach semi-working
    There is a delay between selecting in the dropdown and updating state

--- App.tsx
+++ App.tsx
@@ -1,11 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import * as React from "react";
+import { StyleSheet, Text, View, State } from "react-native";
+import RNPickerSelect from "react-native-picker-select";
+import { Storage } from "./storage";
 
 export default function App() {
+  const storage = new Storage();
+  storage.clearStorage();
+  storage.createTable();
+
+  const [level1, setLevel1] = React.useState();
+  const [level2, setLevel2] = React.useState();
+  const [level3, setLevel3] = React.useState();
+  const [level1Options, setLevel1Options] = React.useState([]);
+  const [level2Options, setLevel2Options] = React.useState([]);
+  const [level3Options, setLevel3Options] = React.useState([]);
+
+  function populateLevel(setLevelxOptions: Function, values) {
+    let results = [];
+    for (let i in values._array) {
+      let result = values.item(i);
+      results.push({ value: result.id, label: result.description });
+    }
+    setLevelxOptions(results);
+  }
+
+  storage.lookupCodesByDepth(1, "0-0-0", (values) =>
+    populateLevel(setLevel1Options, values)
+  );
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <RNPickerSelect
+        onValueChange={(value) => {
+          setLevel1(value);
+          storage.lookupCodesByDepth(2, level1, (values) =>
+            populateLevel(setLevel2Options, values)
+          )
+          console.log("changed after 1", level1, value);
+        }}
+        items={level1Options}
+      />
+      <RNPickerSelect
+        onValueChange={(value) => {
+          setLevel2(value);
+        }}
+        items={level2Options}
+        disabled={level1 == null}
+      />
+      <Text>TEST</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +57,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
